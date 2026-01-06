893a77980ac2680efd7a6ed1c474efb58dfaa287
devtimz01@gmail.com <moses1model@gmail.com>
2026-01-06 18:40:35 +0100

    transcoding-ffmpegsetup

---
+++ src/modules/Transcoding-ffmpeg/transcoding.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, OnModuleDestroy} from '@nestjs/common';
+import { ChildProcess, spawn } from 'child_process';
+import { join } from 'path';
+import EventEmitter from 'events';
+import { mkdir } from 'fs/promises';
+
+@Injectable()
+export class TranscodingService extends EventEmitter implements OnModuleDestroy{
+   private processes = new Map<string,ChildProcess>()
+   private readonly hlsOutputDir = join(process.cwd(),'hls')
+  async startTranscoding(sessionId: string){
+    const outputDir = join(this.hlsOutputDir,sessionId)
+     await mkdir(outputDir,{recursive:true})
+     const ffmpeg = spawn('ffmpeg',[
+        //NB: right her is my input flag
+        '-re', // Real-time input
+        '-i', 'pipe:0', // Read from stdin
+        '-f', 'webm', // Input format
+
+        //video encoding 
+        '-c:v', 'libx264',
+      '-preset', 'veryfast',
+      '-tune', 'zerolatency',
+      '-b:v', '2000k',
+      '-maxrate', '2000k',
+      '-bufsize', '4000k',
+      '-g', '50', // Keyframe interval
+      '-r', '30', // Frame rate
+      '-s', '1280x720', // Resolution
+
+      //audio encoding
+      '-c:a', 'aac',
+      '-b:a', '128k',
+      '-ar', '44100',
+      '-ac', '2',
+
+      //hls setting
+       '-f', 'hls',
+      '-hls_time', '4', // Segment length
+      '-hls_list_size', '6', // Max segments in playlist
+      '-hls_flags', 'delete_segments+append_list',
+      '-hls_segment_filename', join(outputDir,'segment_%03d.ts'),
+      
+      //output
+      join(outputDir,'manisfest.m3u8')
+
+     ],{
+        stdio:['pipe','ignore','pipe']
+     })
+     ffmpeg.stderr.on('data',(data)=>{
+        console.log(`ffmpeg [${sessionId}]:${data.toString()}`)
+     })
+     ffmpeg.on('exit',(code)=>{
+        console.log(`process exit with code ${code}`)
+        this.processes.delete(sessionId)
+     })
+     this.processes.set(sessionId,ffmpeg)
+     return `/hls/${sessionId}/manifest.m3u8` ;
+   }
+   
+} 
