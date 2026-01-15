bae729c26eda017e59c0b5ef0872894aa1fdbb5a
devtimz01@gmail.com <moses1model@gmail.com>
2026-01-15 18:40:37 +0100

    removed unwanted folders in stream branch

--- src/modules/Transcoding-ffmpeg/transcoding.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TranscodingService } from './transcoding.service';
-
-@Module({})
-export class TranscodingModule {
-    exports:[TranscodingService]
-}
